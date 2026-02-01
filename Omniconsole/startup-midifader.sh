54e776b4cce93300c60ce0dd4e303de64c0630d0
Sylvain Centelles <scentelles@gmail.com>
2026-02-01 18:21:02 +0100

    add midi faderscript startup

--- Omniconsole/startup-midifader.sh
+++ Omniconsole/startup-midifader.sh
@@ -1,3 +1,3 @@
 sleep 10
 cd C:\Users\CENTELLES\Projects\LiveServer2\Omniconsole
-python -u midiFader2grandma.py --user toto --password toto
+python -u midiFader2grandma.py --user toto2 --password toto2
